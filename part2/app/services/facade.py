0b0ad840a1561c9d5c690924be626509c56ec201
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-07-16 22:29:41 +0300

    Add __init__.py and Edit facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,8 +1,7 @@
 from app.persistence.repository import InMemoryRepository
-from app.models.user import User
-from app.models.amenity import Amenity
-from app.models.place import Place
-from app.models.review import Review
+
+
+from app.models import User, Amenity, Place, Review
 
 class HBnBFacade:
     """
